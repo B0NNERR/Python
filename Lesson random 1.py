26662c75bd4f1884e13bddd4b40c32f6817c37e2
B0NNERR <b0nnerroff@gmail.com>
2022-01-30 11:46:55 +0300

    Lesson_Randon_1

--- Lesson random 1.py
+++ Lesson random 1.py
@@ -1,14 +1,22 @@
 from random import *
 
-x = randint(1, 100)
+x = randint(1, 3)
 
-n = int(input())
 
-if n > x:
-    print("Слишком много, попробуйте еще раз")
-elif n == x:
-    print("Вы угадали, поздравляем!")
-else:
-    print("Слишком мало, попробуйте еще раз")
 
-print(f"n = {n}, x = {x}")
+
+while True:
+    n = int(input())
+    if n > x:
+        print("Слишком много, попробуйте еще раз")
+        continue
+    elif n == x:
+        print("Вы угадали, поздравляем!")
+        break
+    else:
+        print("Слишком мало, попробуйте еще раз")
+        continue
+    
+
+print(f"\nСлучайное число = {x}"
+      f"\nЧисло введеное пользователем = {n}")
