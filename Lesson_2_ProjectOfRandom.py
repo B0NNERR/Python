3fbadd3065bba596a5b10bcd8acc30c74f055707
B0NNERR <96276011+B0NNERR@users.noreply.github.com>
2022-01-30 16:15:15 +0300

    Update Lesson_2_ProjectOfRandom.py

--- Lesson_2_ProjectOfRandom.py
+++ Lesson_2_ProjectOfRandom.py
@@ -6,31 +6,45 @@ def is_valid(start, end, n):
     else:
         return False
 
+def game(x):
+    
+    flag = False
+    
+    print("Добро пожаловать в числовую угадайку! Введите число")
+    cnt = 0
+    while flag == False:
+        n = input()
+        if is_valid(start, end, n):
+            while flag == False:
+                n = int(n)
+                if n < x:
+                    print("Ваше число меньше загаданного, попробуйте еще разок")
+                    cnt += 1
+                    break
+                elif n > x:
+                    print("Ваше число больше загаданного, попробуйте еще разок")
+                    cnt += 1
+                    break
+                else:
+                    print("Вы угадали, поздравляем! Кол-во попыток: ", cnt)
+                    flag = True
+                    
+                    break
+            
+        else:
+            print("А может быть все-таки введем целое число от 1 до 100?")
+            continue
 
-start, end = 1, 100
-x = randint(start, end)
-flag = False
-print("Добро пожаловать в числовую угадайку")
-flag = False
-while flag == False:
-    n = input()
-    if is_valid(start, end, n):
-        while True:
-            n = int(n)
-            if n < x:
-                print("Ваше число меньше загаданного, попробуйте еще разок")
-                break
-            elif n > x:
-                print("Ваше число больше загаданного, попробуйте еще разок")
-                break
-            else:
-                print("Вы угадали, поздравляем!")
-                print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
-                flag == True
-                break
-        
+ask = ''
+while True:
+    print("Сыграем в угадайку? д - да, н - нет!")
+    ask = input()
+    if ask == 'д':
+        start, end = 1, 100
+        x = randint(start, end)
+        game(x)
+    elif ask == 'н':
+        print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
+        break
     else:
-        print("А может быть все-таки введем целое число от 1 до 100?")
-        continue
-    
-    
+        print("Некорректные данные\n")
